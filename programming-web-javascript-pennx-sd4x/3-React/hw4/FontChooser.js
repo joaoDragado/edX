fd0a3b7a01360cc2184001d4c4df6cdf2cdcb252
joaoDragado <dubmecoco@yahoo.com>
2018-10-25 19:15:14 +0300

    hide/show elements , fontweight checkbox done.

--- programming-web-javascript-pennx-sd4x/3-React/hw4/FontChooser.js
+++ programming-web-javascript-pennx-sd4x/3-React/hw4/FontChooser.js
@@ -2,18 +2,37 @@ class FontChooser extends React.Component {
 
   constructor(props) {
 	  super(props);
-    this.state = { };
+    this.state = {
+      hidden: true,
+      checked: false,
+      weight: 'normal',
+    };
   }
+
+    toggle() {
+      this.setState({hidden : !this.state.hidden});
+    }
+
+    setWeight(){
+      if (this.state.checked)
+        this.setState({weight: 'normal',
+                    checked: false });
+      else
+        this.setState({weight: 'bold',
+                     checked: true });
+
+
+    }
 
   render() {
 
     return(
     <div>
-    <input type="checkbox" id="boldCheckbox" hidden='true'/>
-    <button id="decreaseButton" hidden='true'>-</button>
-    <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-    <button id="increaseButton" hidden='true'>+</button>
-    <span id="textSpan">{this.props.text}</span>
+    <input type="checkbox" id="boldCheckbox" checked={this.state.checked} hidden={this.state.hidden} onClick={this.setWeight.bind(this)}/>
+    <button id="decreaseButton" hidden={this.state.hidden}>-</button>
+    <span id="fontSizeSpan" hidden={this.state.hidden}>{this.props.size}</span>
+    <button id="increaseButton" hidden={this.state.hidden}>+</button>
+    <span style={{ fontWeight: this.state.weight }} id="textSpan" onClick={this.toggle.bind(this)} >{this.props.text}</span>
     </div>
     );
   }
