bffe292f7a894b8a559003baa1ef047d5f9cc745
joaoDragado <dubmecoco@yahoo.com>
2018-10-26 05:14:19 +0300

    hw4 submitted.

--- programming-web-javascript-pennx-sd4x/3-React/hw4/FontChooser.js
+++ programming-web-javascript-pennx-sd4x/3-React/hw4/FontChooser.js
@@ -4,14 +4,21 @@ class FontChooser extends React.Component {
 	  super(props);
     this.state = {
       hidden: true,
-      checked: false,
-      weight: 'normal',
-      size: 16
+      checked: (this.props.bold !== 'false' ? true : false),
+      weight: (this.props.bold !== 'false' ? 'bold' : 'normal'),
+      size: Number(this.props.size),
+      min: Number(this.props.min),
+      max: Number(this.props.max),
+      color : 'black'
     };
   }
 
     toggle() {
       this.setState({hidden : !this.state.hidden});
+    }
+
+    doubleC() {
+      this.setState({size: this.props.size});
     }
 
     setWeight(){
@@ -24,19 +31,31 @@ class FontChooser extends React.Component {
     }
 
     sizeDecr(){
-      let val = this.state.size - 1;
-      if (val >= this.props.min) {
+      let val = this.state.size - 1 ;
+      if (val <= this.state.min) {
         this.setState({
-          size: val
+          size: this.state.min,
+          color : 'red'
+        });
+      } else {
+        this.setState({
+          size: val,
+          color : 'black'
         });
       }
     }
 
     sizeIncr(){
       let val = this.state.size + 1;
-      if (val <= this.props.max) {
+      if (val >= this.state.max) {
         this.setState({
-          size: val
+          size: this.state.max,
+          color : 'red'
+        });
+      } else {
+        this.setState({
+          size: val,
+          color : 'black'
         });
       }
     }
@@ -44,13 +63,14 @@ class FontChooser extends React.Component {
   render() {
     let wgt = this.state.weight;
     let sze = this.state.size;
+    let color = this.state.color;
     return(
     <div>
     <input type="checkbox" id="boldCheckbox" checked={this.state.checked} hidden={this.state.hidden} onClick={this.setWeight.bind(this)}/>
     <button id="decreaseButton" hidden={this.state.hidden} onClick={this.sizeDecr.bind(this)}>-</button>
     <span id="fontSizeSpan" hidden={this.state.hidden}>{sze}</span>
     <button id="increaseButton" hidden={this.state.hidden} onClick={this.sizeIncr.bind(this)}>+</button>
-    <span style={{ fontWeight: wgt, fontSize: sze }} id="textSpan" onClick={this.toggle.bind(this)} >{this.props.text}</span>
+    <span style={{ color: color, fontWeight: wgt, fontSize: sze }} id="textSpan" onClick={this.toggle.bind(this)} onDoubleClick={this.doubleC.bind(this)} >{this.props.text}</span>
     </div>
     );
   }
