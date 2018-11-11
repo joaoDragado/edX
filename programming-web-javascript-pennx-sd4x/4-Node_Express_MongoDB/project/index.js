778a8e0c293c3c7339cf8f952b833784b1db127d
joaoDragado <dubmecoco@yahoo.com>
2018-11-11 16:42:31 +0200

    obtaining user data from HTTP requests via express middle-parser

--- programming-web-javascript-pennx-sd4x/4-Node_Express_MongoDB/project/index.js
+++ programming-web-javascript-pennx-sd4x/4-Node_Express_MongoDB/project/index.js
@@ -1,6 +1,77 @@
 var express = require('express');
 var app = express();
 
+/*
+HTML forms : The form specifies the action and method that the submit button triggers,
+ex. <form action="/handleForm" method="post">
+i.e. submit sends an HTTP request for the url handleForm via the POST method,
+with the data sent in the body of the HTTP request.
+To read the body of the HTTP request in Express, we use the body-parser middleware.
+** npm install body-parser **
+
+*/
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+// the urlencoded method handles data submitted via POST
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/handleForm', (req, res) => {
+  // we now access the body of the request, using the name properties of the <input> elements of our form, i.e. username , animal
+  var name = req.body.username;
+  var animals = req.body.animal; // this is an array
+  . . .
+  res.send('Thank you!');
+});
+
+
+/*
+Request Object Query Properties
+example URL : http://localhost:3000/?name=Lydia&location=United+States
+query starts with ? , params are separated by &, + is converted to whitespace
+*/
+
+app.use('/', (req, res) => {
+  var query = req.query;
+  // print out the JSON object
+  // containing the key/value pairs of our Request.
+  console.log(query);
+
+  var name = query.name;         // 'Lydia'
+  var location = query.location; // 'USA'
+
+  var length = Object.keys(query).length; // 2
+
+  res.write('Hello ' + name +
+            '. I see you are visiting from ' + location + '.');
+});
+
+/*
+alternative form of HTTP Request object
+- parameterized URL
+ex. http://localhost:3000/name/Lydia/location/United States
+*/
+
+app.use('/name/:userName/location/:userLocation',
+(req, res) => {
+  var params = req.params;
+  // print out the JSON object
+  // containing the key/value pairs of our Request.
+  console.log(params);
+
+  var name = params.userName; // 'Lydia'
+  var location = params.userLocation; // 'United States'
+
+  var length = Object.keys(params).length; // 2
+
+  res.write('Hello ' + name +
+            '. I see you are visiting from ' + location + '.');
+});
+
+
+// middleware - subroutes
+
 var logger = (req, res, next) => {
 var url = req.url;
 var time = new Date();
